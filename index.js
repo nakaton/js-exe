316323abf198dab1a00a7a0faf70e79e18b05fc7
Nick <yangyongdun@gmail.com>
2021-07-30 22:00:04 +1200

    JS24

--- index.js
+++ index.js
@@ -400,4 +400,27 @@ function functionFunction(str) {
     }
 }
 
-console.log(functionFunction("Hello")("world"));
+// console.log(functionFunction("Hello")("world"));
+
+
+
+/**
+ * JS24 使用闭包
+ * 实现函数 makeClosures，调用之后满足如下条件：
+ * 1、返回一个函数数组 result，长度与 arr 相同
+ * 2、运行 result 中第 i 个函数，即 result[i]()，结果与 fn(arr[i]) 相同
+ */
+function makeClosures(arr, fn) {
+    let result =[]
+    arr.forEach(element => {
+        const f = function(){
+          return fn(element) 
+        }
+        result.push(f)
+    })
+    return result
+}
+
+// console.log(makeClosures([1, 2, 3], function (x) { 
+// 	return x * x; 
+// })[1]())
